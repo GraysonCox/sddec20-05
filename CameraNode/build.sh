b8357b5f636d6d8ab1645f17cd983a1ff2033520
Ross Thedens <rthedens@iastate.edu>
2020-11-07 16:30:33 -0600

    Rename docker container tag

--- CameraNode/build.sh
+++ CameraNode/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-docker build -t pi .
+docker build -t camera_node .
 
--- CameraNode/start.sh
+++ CameraNode/start.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run -it --network=host --device=/dev/vchiq --device=/dev/video0 pi
+docker run -it --network=host --device=/dev/vchiq --device=/dev/video0 camera_node
